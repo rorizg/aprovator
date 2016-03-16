eefba6da1bf0a4c9dedbd7a7fb501f6033f0a172
Luigi Sica Nery <luigiesn@users.noreply.github.com>
2016-03-16 17:53:42 -0300

    Update database.c

--- code/src/database.c
+++ code/src/database.c
@@ -2,29 +2,35 @@
 #include <string.h>
 
 #include "../include/database.h"
+#include "../include/files.h"
 
 #define MAX_FILENAME_LEN 20
+#define FILE_SEPARATOR ";"
 
-UINT32 searchForDataBase(FILE *dbHeader,char **filenames){
-    UINT32 lineCounter;
-    char tempChar[MAX_FILENAME_LEN];
-
-    for(lineCounter = 0 ; fscanf(dbHeader, "%[^;];", tempChar) != EOF ; lineCounter++){
-    }
-
-    *filenames = (char*)malloc((MAX_FILENAME_LEN*lineCounter) * sizeof(char));
-
-    if(*filenames == NULL){
+UINT32 searchForDataBase(char **filenames){
+    
+    char *data;
+    UINT32 dataAmount , filesCounter, i;
+    
+    dataAmount = loadFile(DB_HEADER_NAME, &data);
+    
+    if(dataAmount == 0){
         return 0;
     }
 
-    fclose(dbHeader);
-
-    for(lineCounter = 0 ; fscanf(dbHeader, "%[^;];", *filenames+lineCounter) != EOF ; lineCounter++){
-            printf("%s\n", *(*filenames+lineCounter));
-    }
-
-    return lineCounter;
+    for(i = 0; i < dataAmount ; i++); 
+        if(data[i] == FILE_SEPARATOR){ /* count files number*/
+            filesCounter++;
+        }
+        *(*filenames) = (char*) malloc(MAX_FILENAME_LEN*filesCounter);
+        
+    char tempName[MAX_FILENAME_LEN];
+    unsigned char j;
+    
+    for(i = 0 ; EOF != scanf("%[^;];", tempName) ;  i++)
+        strcpy(tempName, filename[i])
+           
+    return filesCounter;
 }
 
 void criarBancoDeDados(BancoDeDados *bancoDeDados){
